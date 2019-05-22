ffe75294d26854773614641f7ad31038e52b2412
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2019-05-22 08:29:19 -0700

    added index.js into the drills

---
+++ src/states-drills/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class HelloWorld extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hello world!</p>
+      </div>
+    )
+  }
+}
+
+export 
